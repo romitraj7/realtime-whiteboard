9d4e1df0b632d8e9a3d87d5080a6bfe5d005d865
Romit <romit2884@gmail.com>
2024-04-13 19:49:57 +0530

    create navbar for users and hint component for dispalying tooltip

---
+++ components/hint.tsx
@@ -0,0 +1,29 @@
+import { Tooltip,TooltipContent,TooltipProvider,TooltipTrigger } from "./ui/tooltip";
+
+export interface HintProps{
+    label: string;
+    children:React.ReactNode;
+    side?:"top" | "bottom" | "left" | "right";   
+    align?:"start" | "center" | "end";
+    sideOffest?: number;
+    alignOffest?: number;
+}
+
+const Hint = ({label,children,side,align,sideOffest,alignOffest}:HintProps) => {
+    return(
+        <TooltipProvider>
+        <Tooltip delayDuration={100}>
+            <TooltipTrigger asChild>
+                {children}
+            </TooltipTrigger>
+            <TooltipContent className="text-white bg-black border-black" side={side} align={align} sideOffset={sideOffest} alignOffset={alignOffest}>
+                <p className="font-semibold capitalize">
+                    {label}
+                </p>
+            </TooltipContent>
+        </Tooltip>
+    </TooltipProvider>
+    );
+}
+ 
+export default Hint;
