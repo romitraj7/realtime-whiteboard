9adec2fde1af680d5a65fb5b36da26039c07021a
Romit <romit2884@gmail.com>
2024-04-16 23:17:29 +0530

    update navbar

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { OrganizationSwitcher, UserButton , useOrganization } from "@clerk/nextjs";
-import SearchInput from "./search-input";
 import InviteButton from "./invite-button";
+import { SearchInput } from "./search-input";
 
 const Navbar = () => {
     const {organization} = useOrganization();
