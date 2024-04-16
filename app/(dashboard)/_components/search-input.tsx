b3f0fb51412debc9cf19cc3da0bd89a129068aef
Romit <romit2884@gmail.com>
2024-04-16 23:15:11 +0530

    update search-input component

--- app/(dashboard)/_components/search-input.tsx
+++ app/(dashboard)/_components/search-input.tsx
@@ -1,30 +1,45 @@
 "use client";
-import qs from "query-string";
-import { Search } from "lucide-react";
-import { useDebounceValue} from 'usehooks-ts';
-import { useRouter } from "next/navigation";
-import { useEffect,ChangeEvent,useState } from "react";
+
 import { Input } from "@/components/ui/input";
-const SearchInput = () => {
-    const router = useRouter();
-    const [value,setValue] = useState("");
-    const debounceValue = useDebounceValue(value,500);
-    const handleChange = (e:ChangeEvent<HTMLInputElement>) => {setValue(e.target.value)};
-    useEffect(()=>{
-        const url = qs.stringifyUrl({
-            url: '/',
-            query: {
-                search: debounceValue,
-            },
-        },{skipEmptyString:true ,skipNull:true});
-        router.push(url);
-    },[debounceValue,router])
-    return ( 
-        <div className="w-full relative">
-            <Search className="absolute top-1/2 left-3 transform -translate-y-1/2 text-muted-foreground h-4 w-4"/>
-            <Input className="w-full max-w-[516px] pl-9" placeholder="Search boards" onChange={handleChange} value={value}/>
-        </div>
-     );
+import { Search } from "lucide-react";
+import { useRouter } from "next/navigation";
+import qs from "query-string";
+import { useEffect } from "react";
+import { useDebounceValue } from "usehooks-ts";
+
+export function SearchInput() {
+  const router = useRouter();
+  const [debounceValue, setValue] = useDebounceValue("", 500);
+
+  const handleChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    setValue(e.target.value);
+  };
+
+  useEffect(() => {
+    const url = qs.stringifyUrl(
+      {
+        url: "/",
+        query: {
+          search: debounceValue,
+        },
+      },
+      {
+        skipEmptyString: true,
+        skipNull: true,
+      }
+    );
+
+    router.push(url);
+  }, [debounceValue, router]);
+
+  return (
+    <div className="w-full relative">
+      <Search className="absolute top-1/2 left-3 transform -translate-y-1/2 text-muted-foreground h-4 w-4" />
+      <Input
+        className="w-full max-w-[516px] pl-9"
+        placeholder="Search boards"
+        onChange={handleChange}
+      />
+    </div>
+  );
 }
- 
-export default SearchInput;
